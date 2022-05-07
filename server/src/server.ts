68da0ceedda479e50e92a8eb1fbbcac797df467b
Marcus Vinicius Nunes Bondezan <bondezan@pop-os.localdomain>
2022-05-07 10:49:21 -0300

    added port env var to server

--- server/src/server.ts
+++ server/src/server.ts
@@ -2,13 +2,12 @@ import express from 'express';
 import cors from 'cors';
 import { routes } from './routes';
 
-const SERVER_PORT = 3333;
+const SERVER_PORT = process.env.PORT || 3333;
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
-
 app.use(routes);
 
 app.listen(SERVER_PORT, () => {
